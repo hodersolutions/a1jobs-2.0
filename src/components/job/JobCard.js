21feca634391cdae29b06235477f08654d70d9a6
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-12-21 20:30:15 +0530

    Another CSS
    Another CSS

--- src/components/job/JobCard.js
+++ src/components/job/JobCard.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { green, blue } from '@material-ui/core/colors';
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import BusinessIcon from '@material-ui/icons/Business';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import SubjectIcon from '@material-ui/icons/Subject';
 import MoneyIcon from '@material-ui/icons/Money';
@@ -17,7 +17,7 @@ export class JobCard extends Component {
                     <div className='row'>
                         <div className='col-1'>
                             {/* Get school logo Dynamic */}
-                            <AccountBalanceIcon className="border rounded" color="disabled" style={{ fontSize: 130 }}/>
+                            <BusinessIcon className="border rounded" color="disabled" style={{ fontSize: 130 }}/>
                         </div>
                         <div className='col-11 text-left'>
                             <div className='container-fluid card-data'>
