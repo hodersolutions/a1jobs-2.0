6459094c007118390d902f2091de48fae14c6cce
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-12-26 20:34:20 +0530

    new loading and test
    new loading and test

--- src/components/common/loading/Loading.js
+++ src/components/common/loading/Loading.js
@@ -4,8 +4,8 @@ class Loading extends Component {
     render() {        
         return (
             <Fragment>
-                <div className="d-flex justify-content-center">
-                    <img src={require("../../../static/images/loading/loading-hourglass.gif")} alt="Loading" />
+                <div className="d-flex justify-content-center padding-align">
+                    <img src={require("../../../static/images/loading/loading_gear.gif")} alt="Loading" />
                 </div>
             </Fragment>            
         );
