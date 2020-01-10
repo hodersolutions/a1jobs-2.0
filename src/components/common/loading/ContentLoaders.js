f822772e8a7fcb56d5b46f05d775fdd5024c89d4
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2020-01-10 22:54:53 +0530

    New Loading
    New Loading

--- src/components/common/loading/ContentLoaders.js
+++ src/components/common/loading/ContentLoaders.js
@@ -3,21 +3,28 @@ import ContentLoader from 'react-content-loader';
 
 const FbLoader = () => (
     <Fragment>
-        <ContentLoader height={160} width={400} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
-            <rect x='70' y='15' rx='4' ry='4' width='117' height='6' /> 
-            <rect x='70' y='35' rx='3' ry='3' width='85' height='6' /> 
-            <rect x='0' y='80' rx='3' ry='3' width='350' height='6' /> 
-            <rect x='0' y='100' rx='3' ry='3' width='380' height='6' /> 
-            <rect x='0' y='120' rx='3' ry='3' width='201' height='6' /> 
-            <circle cx='30' cy='30' r='30' />
+        <ContentLoader height={60} width={400} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+            <rect x='70' y='10' rx='4' ry='4' width='300' height='8' /> 
+            <rect x='70' y='30' rx='3' ry='3' width='220' height='4' /> 
+            <rect x='70' y='40' rx='3' ry='3' width='180' height='4' />
+            <rect x='70' y='50' rx='3' ry='3' width='220' height='4' />  
+            <circle cx='30' cy='30' r='25' />
         </ContentLoader>
-        <ContentLoader height={130} width={400}>
-            <rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
-            <rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
-            <rect x="20" y="40" rx="3" ry="3" width="170" height="10" />
-            <rect x="0" y="60" rx="3" ry="3" width="250" height="10" />
-            <rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
-            <rect x="20" y="100" rx="3" ry="3" width="80" height="10" />
+        <ContentLoader height={80} width={400} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+            <circle cx="25" cy="20" r="6" />
+            <rect x="35" y="15" rx="5" ry="5" width="300" height="4" />
+            <rect x="35" y="30" rx="3" ry="3" width="220" height='4' />
+            <rect x="35" y="45" rx="3" ry="3" width="170" height='4' />
+            <rect x="35" y="60" rx="3" ry="3" width="220" height='4' />
+            <rect x="35" y="75" rx="3" ry="3" width="170" height='4' />      
+        </ContentLoader>
+        <ContentLoader height={80} width={400} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+            <circle cx="25" cy="20" r="6" />
+            <rect x="35" y="15" rx="5" ry="5" width="300" height="4" />
+            <rect x="35" y="30" rx="3" ry="3" width="220" height='4' />
+            <rect x="35" y="45" rx="3" ry="3" width="170" height='4' />
+            <rect x="35" y="60" rx="3" ry="3" width="220" height='4' />
+            <rect x="35" y="75" rx="3" ry="3" width="170" height='4' />
         </ContentLoader>
   </Fragment>
 )
