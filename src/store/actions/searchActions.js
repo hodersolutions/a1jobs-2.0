d207e504f9df84c2f8b860c87185a6785e0c4192
hodersolutions <47269382+hodersolutions@users.noreply.github.com>
2020-02-09 16:02:04 +0530

    Update searchActions.js

--- src/store/actions/searchActions.js
+++ src/store/actions/searchActions.js
@@ -1,17 +1,17 @@
 import JobAPI from '../../api/JobAPI';
 
 import { SEARCH_JOB_SUCCESS, 
-         SEARCH_JOB_ERROR } from '../types/searchTypes';
-
-import { SHOW_LOADING, HIDE_LOADING } from '../types/commonTypes';
+         SEARCH_JOB_ERROR,
+         SHOW_SEARCH_JOBS_LOADING,
+         HIDE_SEARCH_JOBS_LOADING } from '../types/searchTypes';
 
 export const searchJobs = (searchParams) => {    
     let api = new JobAPI();
     return (dispatch, getState) => {
-        dispatch({ type: SHOW_LOADING });
+        dispatch({ type: SHOW_SEARCH_JOBS_LOADING });
         api.getJobs(searchParams, {mode: 'cors'})
         .then(response => {
-            dispatch({ type: HIDE_LOADING }); 
+            dispatch({ type: HIDE_SEARCH_JOBS_LOADING }); 
             if (response.status === 'success')
                 dispatch({ type: SEARCH_JOB_SUCCESS, response });            
             else                
