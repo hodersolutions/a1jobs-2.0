3fe9038612b462e035f4ac1f1a4f76af904cc8c7
hodersolutions <47269382+hodersolutions@users.noreply.github.com>
2020-02-09 16:31:46 +0530

    Update CommonAPI.js

--- src/api/CommonAPI.js
+++ src/api/CommonAPI.js
@@ -4,9 +4,36 @@ import BaseAPI from './BaseAPI';
 class UserAPI extends BaseAPI {
 	constructor() {	
         super();	
-        this.getStates = (config={}) =>  axios.get(`${this.url}api/v1/states/all`, config);
-        this.getSubjects = (config={}) =>  axios.get(`${this.url}api/v1/subjects/all`, config);
-        this.getQualifications = (config={}) =>  axios.get(`${this.url}api/v1/qualifications/all`, config);
+        this.getStates = (config={}) =>  axios.get(`${this.url}api/v1/states/all`, config)
+        .then((response) => {
+            return response.data;				
+        })
+        .catch(error => {
+            if (error.response !== undefined)
+                return error.response.data
+            else                
+                return error
+        });
+        this.getSubjects = (config={}) =>  axios.get(`${this.url}api/v1/subjects/all`, config)
+        .then((response) => {
+            return response.data;				
+        })
+        .catch(error => {
+            if (error.response !== undefined)
+                return error.response.data
+            else                
+                return error
+        });
+        this.getQualifications = (config={}) =>  axios.get(`${this.url}api/v1/qualifications/all`, config)
+        .then((response) => {
+            return response.data;				
+        })
+        .catch(error => {
+            if (error.response !== undefined)
+                return error.response.data
+            else                
+                return error
+        });
     }
 }
 
