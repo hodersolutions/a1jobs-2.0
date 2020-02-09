b54b52084b647c4dd5ec299abfee3e9cda567ae3
hodersolutions <47269382+hodersolutions@users.noreply.github.com>
2020-02-09 15:59:14 +0530

    Update ListLoader.js

--- src/components/common/loading/ListLoader.js
+++ src/components/common/loading/ListLoader.js
@@ -9,11 +9,15 @@ const ListLoader = () => (
                     <div className='col-md-12'>
                         <div className='job-loading'>
                             <ContentLoader height={160} width={400} speed={2} primaryColor='#D4EFDF' secondaryColor='#D5F5E3'>                                
-                                <rect x='0' y='10' rx='1' ry='1' width='400' height='25' /> 
-                                <rect x='50' y='50' rx='1' ry='1' width='300' height='15' /> 
-                                <rect x='0' y='75' rx='1' ry='1' width='400' height='25' />
-                                <rect x='50' y='110' rx='1' ry='1' width='300' height='15' />  
-                                <rect x='0' y='135' rx='1' ry='1' width='400' height='25' />
+                                <rect x='0' y='10' rx='1' ry='1' width='400' height='10' /> 
+                                <rect x='50' y='30' rx='1' ry='1' width='300' height='10' /> 
+                                <rect x='0' y='50' rx='1' ry='1' width='400' height='10' />
+                                <rect x='50' y='70' rx='1' ry='1' width='300' height='10' />  
+                                <rect x='0' y='90' rx='1' ry='1' width='400' height='10' />
+                                <rect x='50' y='110' rx='1' ry='1' width='300' height='10' /> 
+                                <rect x='0' y='130' rx='1' ry='1' width='400' height='10' />
+                                <rect x='50' y='150' rx='1' ry='1' width='300' height='10' />  
+                                <rect x='0' y='170' rx='1' ry='1' width='400' height='10' />
                             </ContentLoader>                            
                         </div>
                     </div>
