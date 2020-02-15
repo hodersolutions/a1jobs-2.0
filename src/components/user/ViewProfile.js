1cf2a2414e98b1648b756ef22e78df4f1eead330
hodersolutions <47269382+hodersolutions@users.noreply.github.com>
2020-02-15 08:40:58 +0530

    Changes to v1

--- src/components/user/ViewProfile.js
+++ src/components/user/ViewProfile.js
@@ -8,42 +8,18 @@ import { grey, green } from '@material-ui/core/colors';
 import DetailsLoader from '../common/loading/DetailsLoader';
 import UserAPI from '../../api/UserAPI';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import { gender, teachingmedium, segment, department, circulum } from '../common/Constants';
+import { gender, teachingmedium, segment, department, circulum, resetProfile } from '../common/Constants';
 import NoData from '../common/NoData';
+import moment from 'moment';
+import { DefaultDisplay } from '../common/Helper';
+import NotificationsTimeOut from '../common/contact/Contact';
 
 class ViewAnyProfile extends Component {
 	constructor(props) {
 		super(props);
 		this.api = null;
 		this.state = {
-			profile:{
-                userid: null,
-				firstname: '',
-				middlename: '',
-				lastname: '',
-				fathername: '',
-				gender: 0,    
-				nationality: '',
-				ctc: '',
-				ectc: '',
-				qualification: 0,
-				stateLocation: 0,
-				district: 0,
-				town: 0,
-				totalexperience: 0,
-				circulum: 0,
-				currentorganization: '',
-				department: 0,
-				teachingsubject: 0,
-				teachingmedium: 0,
-				segment: 0,
-				dob:'',
-				email:'',
-				mobile:'',
-				designation:'',
-				address:'',
-				pan:''
-            },
+			profile: resetProfile,
 			loading: false
         }        
 	}    
@@ -74,7 +50,7 @@ class ViewAnyProfile extends Component {
 					});
 				}
 				else {
-					notify.show(response.message, 'error', 5000, 'red');
+					notify.show(response.message, 'error', NotificationsTimeOut, 'red');
 					this.setState({						
 						loading: false
 					});
@@ -85,7 +61,7 @@ class ViewAnyProfile extends Component {
 	
 	render() {
 		const justify = { textAlign: 'justify' }
-		const padding = {paddingRight: '10px'}
+		const padding = { paddingRight: '10px' }
 		const titlePadding = {paddingLeft: '10px'}
 				
 		return (
@@ -114,25 +90,35 @@ class ViewAnyProfile extends Component {
 														<AccountBoxIcon className='no-data-home' style={{ color: grey[500], fontSize: 160 }}/> 														
 													</div>
 													<div>
-														<h2 style={titlePadding}>{this.state.profile.firstname + ' ' + this.state.profile.middlename + ' ' + this.state.profile.lastname}</h2>
+														<h2 style={titlePadding}>
+															{
+																(this.state.profile.firstname + this.state.profile.middlename + this.state.profile.lastname) === '' ?
+																'Unknown User'
+																: this.state.profile.firstname + ' ' + this.state.profile.middlename + ' ' + this.state.profile.lastname
+															}
+														</h2>
 														<div className='container'>
 															<div className='row'>
 																<div className='col-12'>
-																	<span className='icon-briefcase' style={padding}></span>{this.state.profile.designation}
+																	<span className='icon-briefcase' style={padding}></span>{ DefaultDisplay(this.state.profile.designation) }
 																</div>
 															</div>
 															<div className='row'>
 																<div className='col-12'>
-																	<span className='icon-room' style={padding}></span>{this.state.profile.stateLocation},&nbsp;{this.state.profile.district},&nbsp;{this.state.profile.town}
+																	<span className='icon-room' style={padding}></span>
+																	{ DefaultDisplay(this.state.profile.town) },&nbsp;{ DefaultDisplay(this.state.profile.district) },&nbsp;{ DefaultDisplay(this.state.profile.stateLocation) }
 																</div>
-															</div>
-															{/* <div className='row'>
-																<div className='col-12'>
-																	Expected CTC: {this.state.profile.ectc}
-																</div>
-															</div> */}
+															</div>															
 														</div>
 													</div>										
+												</div>
+											</div>
+											<div className='col-lg-4'>
+												<div className='row'>
+													<div className='col-6'/>																
+													<div className='col-6'>
+														<Link to='/edituser' className='btn btn-primary border-width-2 d-lg-inline-block sign-in-link'><span className='mr-2 icon-pencil'></span>Edit Profile</Link>
+													</div>																							
 												</div>
 											</div>
 										</div>
@@ -144,34 +130,46 @@ class ViewAnyProfile extends Component {
                                                     <div className='container'>
                                                         <div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'>Date Of Birth:</strong> {this.state.profile.dob}
+                                                                <strong className='text-black'>Date Of Birth: </strong>
+																{ DefaultDisplay(moment(this.state.profile.dob, "dd/MM//yyyy").format('Do MMMM, YYYY')) }
                                                             </div>
                                                         </div>
+														<br/>
                                                         <div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'>Address:</strong> {this.state.profile.address}
+                                                                <strong className='text-black'>Address: </strong> 
+																{ DefaultDisplay(this.state.profile.address) }
                                                             </div>
                                                         </div>
+														<br/>
 														<div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'>PAN:</strong> {this.state.profile.pan}
+                                                                <strong className='text-black'>PAN: </strong>
+																{ DefaultDisplay(this.state.profile.pan) }
                                                             </div>
                                                         </div>
+														<br/>
                                                         <div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'> Gender:</strong> {gender.filter(gd => parseInt(gd.id) === parseInt(this.state.profile.gender))[0].name}
+                                                                <strong className='text-black'>Gender: </strong> 
+																{ DefaultDisplay(gender.filter(gd => parseInt(gd.id) === parseInt(this.state.profile.gender))[0].view) }
                                                             </div>
                                                         </div>
+														<br/>
                                                         <div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'>Father Name:</strong> {this.state.profile.fathername}
+                                                                <strong className='text-black'>Father Name: </strong>
+																{ DefaultDisplay(this.state.profile.fathername) }
                                                             </div>
                                                         </div>
+														<br/>
                                                         <div className='row'>
                                                             <div className='col-12'>
-                                                                <strong className='text-black'> Nationality:</strong> {this.state.profile.nationality}
+                                                                <strong className='text-black'> Nationality: </strong>
+																{ DefaultDisplay(this.state.profile.nationality) }
                                                             </div>
                                                         </div>
+														<br/>
                                                     </div>
                                                 </div>
                                                 </div>
@@ -181,12 +179,15 @@ class ViewAnyProfile extends Component {
                                                             <div className='container'>
                                                                 <div className='row'>
                                                                     <div className='col-12'>
-                                                                        <strong className='text-black'>Email:</strong> {this.state.profile.email}
+                                                                        <strong className='text-black'>Email: </strong>
+																		{ DefaultDisplay(this.state.profile.email) }
                                                                     </div>
                                                                 </div>
+																<br/>
                                                                 <div className='row'>
                                                                     <div className='col-12'>
-                                                                        <strong className='text-black'> Mobile:</strong> {this.state.profile.mobile}
+                                                                        <strong className='text-black'> Mobile: </strong>
+																		{ DefaultDisplay(this.state.profile.mobile) }
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -196,8 +197,9 @@ class ViewAnyProfile extends Component {
                                                 <h3 className='h5 d-flex align-items-center mb-4 text-primary'><span className='icon-book mr-3'></span>Education + Experience</h3>
                                                     <div className='container'>
                                                         <div className='row'>
-                                                            <div className='col-14'>
-                                                                <strong className='text-black'>Total years of experience:</strong> {this.state.profile.totalexperience}
+                                                            <div className='col-12'>
+                                                                <strong className='text-black'>Total years of experience: </strong>
+																{ DefaultDisplay(this.state.profile.totalexperience) } years
                                                             </div>
                                                         </div>
                                                     </div>
@@ -207,32 +209,25 @@ class ViewAnyProfile extends Component {
                                                 <div className='bg-light p-3 border rounded mb-4'>
                                                 <h3 className='text-primary  mt-3 h5 pl-3 mb-3 '>Profession Summary</h3>
                                                 <ul className='list-unstyled pl-3 mb-0'>
-                                                    <li className='mb-2'><strong className='text-black'>Current Designation:</strong> {this.state.profile.designation}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Current Organization:</strong> {this.state.profile.currentorganization}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Teaching Subject:</strong> {this.state.profile.teachingsubject}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Teaching Medium:</strong> {teachingmedium.filter(tm => parseInt(tm.id) === parseInt(this.state.profile.teachingmedium))[0].name}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Circulum:</strong> {circulum.filter(cr => parseInt(cr.id) === parseInt(this.state.profile.circulum))[0].name}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Department:</strong> {department.filter(dp => parseInt(dp.id) === parseInt(this.state.profile.department))[0].name}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Segment:</strong> {segment.filter(sg => parseInt(sg.id) === parseInt(this.state.profile.segment))[0].name}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Current CTC:</strong> {this.state.profile.ctc}</li>
-                                                    <li className='mb-2'><strong className='text-black'>Expected CTC:</strong> {this.state.profile.ectc}</li>
+												<li className='mb-2'><strong className='text-black'>Current Designation:</strong> { DefaultDisplay(this.state.profile.designation) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Current Organization:</strong> { DefaultDisplay(this.state.profile.currentorganization) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Teaching Subject:</strong> { DefaultDisplay(this.state.profile.teachingsubject) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Teaching Medium:</strong> { DefaultDisplay(teachingmedium.filter(tm => parseInt(tm.id) === parseInt(this.state.profile.teachingmedium))[0].view) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Circulum:</strong> { DefaultDisplay(circulum.filter(cr => parseInt(cr.id) === parseInt(this.state.profile.circulum))[0].view) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Department:</strong> { DefaultDisplay(department.filter(dp => parseInt(dp.id) === parseInt(this.state.profile.department))[0].view) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Segment:</strong> { DefaultDisplay(segment.filter(sg => parseInt(sg.id) === parseInt(this.state.profile.segment))[0].view) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Current CTC:</strong> &#x20b9; { DefaultDisplay(this.state.profile.ctc) }</li>
+                                                    <li className='mb-2'><strong className='text-black'>Expected CTC:</strong> &#x20b9; { DefaultDisplay(this.state.profile.ectc) }</li>
                                                 </ul>
                                                 </div>			
-										      </div>
-											  <div className='col-lg-4'>
-												<div className='row'>											
-													<div className='col-6'>
-														<Link to='/edituser' className='btn btn-primary border-width-2 d-lg-inline-block sign-in-link'><span className='mr-2 icon-sign-in'></span>Edit Profile</Link>
-													</div>
-												</div>
-											  </div>
+										      </div>											 
                                         </div>
 									</div>
 								</section>
 							)
 							:
 							(
-								<NoData tag={
+								<NoData key = '1' tag={
 									<p>User details do not exist, click <Link to='/signin'>here</Link> to <ExitToAppIcon className='no-data-home' style={{ color: green[500], fontSize: 40, verticalAlign: -7 }}/> login.</p>
 								} />
 							)
